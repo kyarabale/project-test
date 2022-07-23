8f9b9abfaa649c9ef1868f1915fa5de375d2d174
kyara <kyaramailbox@163.com>
2022-07-23 10:46:12 +0800

    second commit

--- src/main/java/com/kyara/Demo.java
+++ src/main/java/com/kyara/Demo.java
@@ -5,6 +5,7 @@ package com.kyara;
 public class Demo {
     public String say(String name){
         System.out.println("hello " + name);
+        System.out.println("hello git ~");
         return "hello " + name;
     }
 }
